dd7e98974dd9e17e9a8c1d11082da7fc68a9e30c
Christian Tobias <cwtobias6@gmail.com>
2017-10-17 21:59:36 -0700

    Begin Segues on Clima app. And Add Segue example app.

---
+++ Segues/Segues/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Segues
+//
+//  Created by Christian Tobias on 10/17/17.
+//  Copyright © 2017 Christian Tobias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        performSegue(withIdentifier: "goToSecondScreen", sender: self)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToSecondScreen" {
+            let destinationVC = segue.destination as! SecondViewController
+            
+            destinationVC.textPassedOver = textField.text!
+        }
+    }
+}
+
