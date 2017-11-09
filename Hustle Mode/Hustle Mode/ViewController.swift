53d290250b0a1927b201daf288e65efef084d568
Christian Tobias <cwtobias6@gmail.com>
2017-11-08 17:30:21 -0800

    Init repo with layout completed

---
+++ Hustle Mode/Hustle Mode/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Hustle Mode
+//
+//  Created by Christian Tobias on 11/8/17.
+//  Copyright © 2017 Christian Tobias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var darkBlueBG: UIImageView!
+    @IBOutlet weak var powerButton: UIButton!
+    @IBOutlet weak var cloudHolder: UIView!
+    @IBOutlet weak var rocket: UIImageView!
+    @IBOutlet weak var onLabel: UILabel!
+    @IBOutlet weak var hustleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func powerButtonPressed(_ sender: Any) {
+    }
+    
+}
+
