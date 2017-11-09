0e31b1a1088089540fde3da46c25a6ab5d8cb957
Christian Tobias <cwtobias6@gmail.com>
2017-11-08 18:10:26 -0800

    Add image animation

--- Hustle Mode/Hustle Mode/ViewController.swift
+++ Hustle Mode/Hustle Mode/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -17,12 +18,36 @@ class ViewController: UIViewController {
     @IBOutlet weak var onLabel: UILabel!
     @IBOutlet weak var hustleLabel: UILabel!
     
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = Bundle.main.path(forResource: "hustle-on", ofType: "wav")!
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player.prepareToPlay()
+        } catch let error as NSError {
+            print(error.description)
+        }
     }
 
     @IBAction func powerButtonPressed(_ sender: Any) {
+        powerButton.isHidden = true
+        cloudHolder.isHidden = false
+        darkBlueBG.isHidden = false
+        
+        player.play()
+        
+        UIView.animate(withDuration: 2.3, animations: {
+            self.rocket.frame = CGRect(x: 0, y: 20, width: 240, height: 128)
+        }) { (finished) in
+            self.hustleLabel.isHidden = false
+            self.onLabel.isHidden = false
+        }
     }
     
 }
